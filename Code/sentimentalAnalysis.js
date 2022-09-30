be18429165aff7baa1f3db3fa2015e4a794b3b87
HuyTao100 <32138897+HuyTao100@users.noreply.github.com>
2022-09-29 20:22:29 -0700

    Sentimental Done

--- Code/sentimentalAnalysis.js
+++ Code/sentimentalAnalysis.js
@@ -10,6 +10,7 @@
 */
 
 var fs = require('fs');
+const {cse408_bow} = require('./cse408_bow.js');
 
 const lexicon = '../Data/wordwithStrength.txt';
 //get every line from lexicon
@@ -23,10 +24,56 @@ for (let i = 0; i < lexiconWords.length; i++){
     weights[lexiconWords[i]] = lexiconValues[i];
 }
 
-
 function sentimentalAnalysis(filepath){
-    console.log(weights);
+    const {feat_vec} = cse408_bow(filepath, lexiconWords);
+    let score = 0;
+    for (const word in feat_vec){
+        if (word in weights){
+            score += weights[word] * feat_vec[word];
+        }
+    }
+    
+    if (score > 0){
+        if (score > 0.7){
+            console.log(score);    
+            console.log("Highly Positive");
+        }
+        else {
+            console.log(score);
+            console.log("Positive");
+        }
+    }
+
+    if (score < 0){
+        if (score < -0.7){
+            console.log(score);
+            console.log("Highly Negative");
+        }
+        else {
+            console.log(score);
+            console.log("Negative");
+        }
+    }
+
+    if (score == 0){
+        console.log("Neutral");
+    }
 }
-sentimentalAnalysis(null);
 
+for (let i = 1; i < 41; i++) {
+    if (i < 10){
+        sentimentalAnalysis(`../Data/pos/00${1}.txt`);
+    }
+    else {
+        sentimentalAnalysis(`../Data/pos/0${i}.txt`);
+    }
+}
 
+for (let i = 1; i < 41; i++) {
+    if (i < 10){
+        sentimentalAnalysis(`../Data/neg/00${i}.txt`);
+    }
+    else {
+        sentimentalAnalysis(`../Data/neg/0${i}.txt`);
+    }
+}
